373202347529a4a60b62113bc131e667ca45ccc8
Peter Siegesmund <peter.siegesmund@gmail.com>
2015-04-09 15:11:29 +0000

    ...

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 docker build -t elasticsearch .
-docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -v $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/data:/data -v $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/config:/elasticsearch/config --restart always elasticsearch
+docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -v $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/data:/data --restart always elasticsearch
